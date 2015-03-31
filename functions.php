9329f3d859cc4e2bddb1a74e1c4b8fdf5e8b8190
Kenneth Hall <klhall1987@gmail.com>
2015-03-31 18:40:40 -0400

    I'm needing some help with logic.

--- functions.php
+++ functions.php
@@ -3,3 +3,5 @@
 function check( $function_to_test, $test, $accepted_answer ) {
   return ( $accepted_answer == call_user_func( $function_to_test, $test) ) ? 'correct' : 'incorrect' ;
 }
+
+
--- roman-numerals.php
+++ roman-numerals.php
@@ -6,6 +6,20 @@
  * @param string $numerals
  * @return integer
  */
+ 
 function roman_numerals( $numerals ) {
+	$numerals = strtolower( $numerals );
+	$arabic_Numbers = str_split( $numerals );
+	var_dump( $arabic_Numbers );
+	foreach($arabic_Numbers as $numerals) {
+		if($numerals == 'i'){
+			$numerals = 1;
+		} elseif($numerals == 'v') {
+			$numerals = 5;
+		} elseif ($numerals == 'x') {
+			$numerals = 10;
+		}
+	}
 
+	return($numerals);
 }
