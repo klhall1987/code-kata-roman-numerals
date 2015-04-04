0100bb50aad5d3571841e7b6ce0d56fe31fb72bf
Kenneth Hall <klhall1987@gmail.com>
2015-04-03 20:57:19 -0400

    I have broken it so badly it says everything is correct. I am working on a fix. I thought this was pretty funny though.

--- roman-numerals.php
+++ roman-numerals.php
@@ -8,18 +8,31 @@
  */
  
 function roman_numerals( $numerals ) {
+	// Set the numerals array to all lower case.  
 	$numerals = strtolower( $numerals );
-	$arabic_Numbers = str_split( $numerals );
-	var_dump( $arabic_Numbers );
-	foreach($arabic_Numbers as $numerals) {
-		if($numerals == 'i'){
-			$numerals = 1;
-		} elseif($numerals == 'v') {
-			$numerals = 5;
-		} elseif ($numerals == 'x') {
-			$numerals = 10;
-		}
-	}
 
-	return($numerals);
+	// Split the numerals array into an array of stings for each line. 
+	$arabic_Numbers = str_split( $numerals );
+
+	// Converting the arrays into strings 
+	$imp = implode('' , $arabic_Numbers);
+
+	//count to find the position of the numeral. 
+	$result = strlen($imp);
+
+	//varible for I.
+	$empty_Numeral = '';
+
+	//find the position of numeral I. 
+	$str_pos = stripos($imp, $empty_Numeral);
+
+	// being used for debug, Remove once code is complete
+	var_dump($str_pos);
+
+	$answer = 0;
+
+	if($empty_Numeral = 'i' && $str_pos <= 0  ){
+
+	}
+	return $empty_Numeral;
 }
